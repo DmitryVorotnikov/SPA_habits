f672d2614990bbffa86ca3f92bb58949a79056e4
dmitryvorot <vorotnikov7009@mail.ru>
2023-12-17 18:05:18 +0800

    Added User Model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
 
 
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    chat_id = models.CharField(max_length=100, verbose_name='chat_id', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
