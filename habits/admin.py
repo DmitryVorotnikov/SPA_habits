c70cd7db543a4a0d2b1bcf65300cd7038a36cb72
dmitryvorot <vorotnikov7009@mail.ru>
2023-12-17 23:25:06 +0800

    Added settings for the admin panel

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import UsefulHabit, PleasantHabit
+
+
+@admin.register(UsefulHabit)
+class UsefulHabitAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'action',
+        'action_time_in_minutes',
+        'started_at',
+        'location',
+        'pleasant_habit',
+        'reward',
+        'periodicity',
+        'is_published',
+    )
+    list_filter = ('user', 'periodicity', 'is_published',)
+    search_fields = ('user', 'action', 'location',)
+
+
+@admin.register(PleasantHabit)
+class PleasantHabitAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'action',
+        'action_time_in_minutes',
+        'location',
+        'is_published',
+    )
+    list_filter = ('user', 'is_published',)
+    search_fields = ('user', 'action', 'location',)
