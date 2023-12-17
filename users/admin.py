1323c65bebb914e65408b0176f6ff7515f7e8d6e
dmitryvorot <vorotnikov7009@mail.ru>
2023-12-17 18:51:34 +0800

    Configured admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'chat_id',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+        'last_login',
+        'date_joined',
+        'password',
+    )
+    list_filter = ('first_name', 'last_name',)
+    search_fields = ('first_name', 'last_name', 'email', 'chat_id',)
