9084ca85dd60d7dfea822183ad0cfa1c8ad7a129
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-01-26 19:41:12 +0800

    Modified deploy.sh for CI/CD

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,18 @@ sudo systemctl stop postgresql
 sudo systemctl stop spa_habits
 sudo systemctl restart nginx
 
+# Добавляем права
+sudo usermod -aG docker gitlab-runner
+sudo chown -R gitlab-runner /home/gitlab-runner/
+sudo chmod -R a+rx /home/gitlab-runner/
+
 # Перезапускаем Docker-Compose
 docker-compose down
 docker-compose build
 docker-compose up -d
 docker-compose exec web_api python manage.py collectstatic --noinput
 
-# Добавляем права
+# Добавляем права заново
 sudo usermod -aG docker gitlab-runner
 sudo chown -R gitlab-runner /home/gitlab-runner/
 sudo chmod -R a+rx /home/gitlab-runner/
