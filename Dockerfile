b2cff31b7052a211f912215582afb86219ca4bfd
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-01-26 19:13:25 +0800

    Modified deploy.sh for CI/CD

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,3 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY . .
-
-RUN python manage.py collectstatic --noinput
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 docker-compose down
 docker-compose build
 docker-compose up -d
+docker-compose exec web_api python manage.py collectstatic
