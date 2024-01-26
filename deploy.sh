d66bb8e9a03174eb3288e4a659469a3693b220db
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-01-26 19:17:34 +0800

    Modified deploy.sh for CI/CD

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker-compose down
 docker-compose build
 docker-compose up -d
-docker-compose exec web_api python manage.py collectstatic
+docker-compose exec web_api python manage.py collectstatic --noinput
