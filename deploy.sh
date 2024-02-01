051a04b3d0c9df54950b58864c5d9188340ca18a
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-02-02 00:47:27 +0800

    Modified deploy.sh for CI/C

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,9 @@ docker-compose up -d
 # Добавляем права заново
 sudo usermod -aG docker gitlab-runner
 sudo chown -R gitlab-runner /home/gitlab-runner/
+sudo chown -R gitlab-runner:gitlab-runner /home/gitlab-runner/
 sudo chmod -R a+rwx /home/gitlab-runner/
+sudo chmod -R 755 /home/gitlab-runner/
 
 # Перезагружаем nginx
 sudo systemctl restart nginx
@@ -27,7 +29,9 @@ sudo systemctl restart nginx
 # Добавляем права заново
 sudo usermod -aG docker gitlab-runner
 sudo chown -R gitlab-runner /home/gitlab-runner/
+sudo chown -R gitlab-runner:gitlab-runner /home/gitlab-runner/
 sudo chmod -R a+rwx /home/gitlab-runner/
+sudo chmod -R 755 /home/gitlab-runner/
 
 #Собираем статику
 #sudo docker-compose exec web_api python manage.py collectstatic --noinput
