bff30db5f62065cc9739b045b922ba5c440cc180
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-02-02 02:28:00 +0800

    Modified for CI/C

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,7 @@
-# Останавливаем и перезагружаем сервисы
-sudo systemctl stop postgresql
-sudo systemctl stop spa_habits
-sudo systemctl restart nginx
-
-# Добавляем права
-sudo usermod -aG docker gitlab-runner
-sudo chown -R gitlab-runner /home/gitlab-runner/
-sudo chmod -R a+rwx /home/gitlab-runner/
-
-# Останавливаем и перезагружаем сервисы
-sudo systemctl restart nginx
-
 # Перезапускаем Docker-Compose
 docker-compose down
 docker-compose build
 docker-compose up -d
-
-# Добавляем права заново
-sudo usermod -aG docker gitlab-runner
-sudo chown -R gitlab-runner:gitlab-runner /home/gitlab-runner/
-sudo chmod -R a+rwx /home/gitlab-runner/
-
-# Перезагружаем nginx
-sudo systemctl restart nginx
-
-# Добавляем права заново
-sudo usermod -aG docker gitlab-runner
-sudo chown -R gitlab-runner:gitlab-runner /home/gitlab-runner/
-sudo chmod -R a+rwx /home/gitlab-runner/
 
 #Собираем статику
 #sudo docker-compose exec web_api python manage.py collectstatic --noinput
